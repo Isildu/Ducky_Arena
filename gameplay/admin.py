ff3b17276c51f9a7e4218cb4ef929cc15a273e7d
Martret92 <jaime.martret@gmail.com>
2026-05-28 22:21:11 +0200

    Add initial gameplay seed data and admin setup

--- gameplay/admin.py
+++ gameplay/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Character)
+admin.site.register(Ability)
+admin.site.register(GameMap)
+admin.site.register(Match)
+admin.site.register(MatchPlayer)
+admin.site.register(PlayerProfile)
+admin.site.register(PlayerFriend)
+admin.site.register(Cosmetic)
+admin.site.register(PlayerCosmetic)
+admin.site.register(DailyQuest)
+admin.site.register(PlayerQuest)
+admin.site.register(QuestionCategory)
+admin.site.register(Question)
+admin.site.register(AnswerOption)
